ab068e19117f43b95d15df5f8e7977aabe9deb50
Unik <unikbahadur1852@gmail.com>
2025-06-08 20:01:50 +0545

    First oop concept

---
+++ tut1.py
@@ -0,0 +1,12 @@
+class hello:
+    def __init__(self):
+        print('First')
+        self.a = 1
+        self.b = 3
+
+
+    def hi(self,name):
+        print(f'My name is {name}')
+
+hel  = hello()
+hel.hi('Unik')
