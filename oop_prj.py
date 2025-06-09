1693173db51de454c1b74a07625ec23c21570e0d
Unik <unikbahadur1852@gmail.com>
2025-06-09 12:59:25 +0545

    2.modification

--- oop_prj.py
+++ oop_prj.py
@@ -15,9 +15,10 @@ class coverbook:
                            ''')
         
         if user_input =='1':
-            pass
+            self.signup()
+            self.menu()
         elif user_input =='2':
-            pass
+            self.signin()
         elif user_input=='3':
             pass
         elif user_input=='4':
@@ -25,5 +26,29 @@ class coverbook:
         else:
             pass
 
+    
+    def signup(self):
+        email = input('Enter your email:')
+        password = input('Enter your password:')
+        self.user_name = email
+        self.password = password
+        print('You successfully signed-up:')
+
+
+    def signin(self):
+        if self.user_name =='' and self.password =='':
+            print('You have not signed up please kindly sign in:')
+
+        else:
+            user = input('Enter user name:')
+            pwd = input('Enter your password:')
+
+            if user == self.user_name and pwd == self.password:
+                print('You signed up successfully:')
+                self.loggedin ==True
+
+            else:
+                print('Please enter correct information:')
+                self.menu()
 obj = coverbook()
 
